913bf938c4d3160cdc286ca970f8c251179992ba
Roman Kuptsov <romankuptsov@icloud.com>
2023-06-23 20:15:21 +0200

    solved Lab

--- index.js
+++ index.js
@@ -1,23 +1,53 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
+const data = require("./data");
 
-// Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
-const data = require('./data');
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+let newRecipe = {
+  title: "Smoothie",
+  level: "Easy Peasy",
+  ingredients: ["apple", "banana", "strawberry", "jogurt", "corn flakes"],
+  cuisine: "Worldwide",
+  dishType: "snack",
+  image:
+    "https://www.evolvingtable.com/wp-content/uploads/2022/12/How-to-Make-Smoothie-1.jpg",
+  duration: 5,
+  creator: "Gym People",
+};
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.create(newRecipe);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((result) => {
+    console.log(`New Recipe added: ${result.title}`);
+  })
+  .then((result) => {
+    return Recipe.insertMany(data);
+  })
+  .then(() => {
+    const filter = { title: "Rigatoni alla Genovese" };
+    const update = { duration: 100 };
+
+    return Recipe.findOneAndUpdate(filter, update);
+  })
+  .then(() => {
+    console.log(`The recipe Carrot Cake is updated`);
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .then((result) => {
+    console.log(`${result.title} recipe was deleted`);
+    return mongoose.connection.close();
+  })
+  .then(() => console.log(`connection closed`))
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+
+module.exports = newRecipe;
